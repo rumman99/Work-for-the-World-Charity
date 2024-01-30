a320b4f767a9140857fe82443bdc22a83b105795
rumman99 <tasnimrumman369@gmail.com>
2024-01-31 04:15:46 +0600

    remove paragraph from task body

--- src/Components/Tasks/Tasks.tsx
+++ src/Components/Tasks/Tasks.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import taskList from "../../fakeData/fakeData";
 
 interface TaskProps{
     task:{
@@ -11,12 +13,13 @@ interface TaskProps{
 const Tasks = ({task:{name, img, id}}:TaskProps) => {
 
     return (
+        <>
         <Link to={'/volunteer/'+id}><div className="hover:scale-90 lg:w-64 lg:ml-28 md:w-40 md:ml-4 bg-base-100 shadow-2xl mt-10 p-2 hover:bg-blue-200 hover:cursor-pointer rounded-ss-xl">
-            <h2 className="card-title">{name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <h2 className="card-title justify-center">{name}</h2>
             <img style={{height:'300px'}} src={img} alt="Shoes" />
-        </div></Link>
-      
+        </div>
+        </Link>
+      </>
     );
 };
 
